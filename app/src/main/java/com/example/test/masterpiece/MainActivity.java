d1095f0159d36d18e62c5e4727233e3e5f3d7dc9
Kimsihwan <namid882@gmail.com>
2018-04-13 23:23:06 +0900

    주석

--- app/src/main/java/com/example/test/masterpiece/MainActivity.java
+++ app/src/main/java/com/example/test/masterpiece/MainActivity.java
@@ -17,16 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("명화 선호도 투표");
 
+        // 필요한 변수를 선언한다.
+
+        // 그림을 클릭할 때마다 투표수를 저장할 9개짜리 배열을 선언하고 0으로 초기화한다.
         final int voteCount[] = new int[9];
         for (int i =0; i < 9; i ++)
             voteCount[i] = 0;
 
+        // 이미지뷰 위젯을 저장할 9개 짜리 배열을 선언한다.
         ImageView image[] = new ImageView[9];
+
+        // 이미지뷰 위젯의 id인 R.id.pic1 ~ pic9를 저장한 배열을 선언한다.
         Integer imageId[] = {R.id.pic1, R.id.pic2, R.id.pic3, R.id.pic4, R.id.pic5, R.id.pic6, R.id.pic7, R.id.pic8, R.id.pic9};
+
+
         final String imgName[] = {"얍얍", "조이", "조이아이린", "조이아이린2", "아이린", "박성수", "김재현", "모재영", "김진우"};
 
 
         // 이미지를 클릭할때마다 해당 이미지의 이름과 누적된 투표수도 토스트 메시지로 보여줌
+        // 이미지뷰가 9개나 되므로 반복문을 사용
         for (int i = 0; i < imageId.length; i++){
             final int index;
             index = i;
@@ -40,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+
+        // 인텐트를 생성하고, 인텐트에 투표수 배열과 그림 이름 배열을 넣은 후에 결과 액티비티를 호출
         Button btnFinish = (Button) findViewById(R.id.btnResult);
         btnFinish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
 
         Button btnThrid = (Button) findViewById(R.id.btn3);
         btnThrid.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/test/masterpiece/result.java
+++ app/src/main/java/com/example/test/masterpiece/result.java
@@ -16,26 +16,32 @@ public class result extends AppCompatActivity {
         setContentView(R.layout.result);
         setTitle("투표 결과");
 
+        //메인 액티비티에서 보낸 인텐트를 받고, 넘겨 받은 투표 결과 배열과 그림 이름을 배열을 저장
         Intent intent = getIntent();
         int[] voteResult = intent.getIntArrayExtra("VoteCount");
         String[] imageName = intent.getStringArrayExtra("ImageName");
 
+
+        // 텍스트뷰 9개와 레이팅바 9개의 위젯 변수 배열을 선언
         TextView tv[] = new TextView[imageName.length];
         RatingBar rbar[] = new RatingBar[imageName.length];
 
+        // 텍스트뷰 id를 저장한 배열 변수, 레이팅바의 id를 저장한 배열 변수를 선언한다.
         Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
         Integer rbarID[] = {R.id.rbr1, R.id.rbr2, R.id.rbr3, R.id.rbr4, R.id.rbr5, R.id.rbr6, R.id.rbr7, R.id.rbr8, R.id.rbr9};
+
 
         for(int i = 0; i < voteResult.length; i++){
             tv[i] = (TextView) findViewById(tvID[i]);
             rbar[i] = (RatingBar) findViewById(rbarID[i]);
         }
-
+        // 텍스트뷰 위젯 변수에 넘겨받은 그림을 적용한다. 레이팅바에는 넘겨 받은 투표 결과를 적용한다.
         for (int i = 0; i < voteResult.length; i++){
             tv[i].setText(imageName[i]);
             rbar[i].setRating((float) voteResult[i]);
         }
 
+        // 버튼을 클릭하면 서브 액티비티를 종료한다.
         Button btnReturn = (Button) findViewById(R.id.btnReturn);
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
